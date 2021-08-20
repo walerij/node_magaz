4c982b3a109e0c8488228cab7ff8f2a36cb9b794
Zhdanov Valery <becwal@yandex.ru>
2021-08-20 07:08:42 +0300

    Подключение express и основные модули

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Электронный магазин")
+const express = require("express")
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Главная")
+})
+
+app.get('/product',(req,res)=>{
+    res.send("Товары")
+})
+
+
+const PORT = process.env.PORT||3000
+
+app.listen(PORT,()=>{
+    console.log("server started on port:"+PORT)
+})

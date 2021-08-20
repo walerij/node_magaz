f5298871bea37479fdcb07b9a8721bafab05ed36
Zhdanov Valery <becwal@yandex.ru>
2021-08-20 07:37:08 +0300

    Режимы работы с товаром: добавление, редактирование, просмотр, вывод статуса

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,29 @@ const router = Router()
 router.get("/", async (req,res)=>{
    await res.send("Список товаров")
 }
-           
 )
 
+router.get("/list",async (req,res)=>{
+    await res.send("Вывод списка товаров")
+}         
+)
+
+router.get("/add",async (req,res)=>{
+    await res.send("Добавление товара")
+}         
+)
+router.get("/:id/edit",async (req,res)=>{
+    await res.send("Редактирование одного товара")
+}         
+)
+
+router.get("/:id/view",async (req,res)=>{
+    await res.send("Просмотр одного товара")
+}         
+)
+
+router.get("/:id/status",async (req,res)=>{
+    await res.send("Новый статус одного товара")
+}         
+)
 module.exports = router
